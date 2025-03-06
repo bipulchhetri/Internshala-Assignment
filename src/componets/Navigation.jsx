36b4a6d376b1677da9d36035d6c43022cad9cb7a
bipulchhetri <bipulchhetri80@gmail.com>
2025-03-06 23:19:52 +0530

    done

--- src/componets/Navigation.jsx
+++ src/componets/Navigation.jsx
@@ -1,24 +1,3 @@
-// import React from 'react';
-
-// function Navigation() {
-//   return (
-//     <nav className="navigation">
-//       <div className="logo">M.</div>
-//       <div className="nav-links">
-//         <a href="#home" className="active">Home</a>
-//         <a href="#about">About</a>
-//         <a href="#projects">Projects</a>
-//         <a href="#contact">Contact</a>
-//       </div>
-//       <div className="nav-links-right">
-//         <a href="#hire" className="hire-button">HIRE ME</a>
-//       </div>
-//     </nav>
-//   );
-// }
-
-// export default Navigation;
-
 import React, { useState } from "react";
 import "./Navigation.css";
 
@@ -31,10 +10,8 @@ const Navigation = () => {
 
   return (
     <nav className="navbar">
-      {/* Left - Logo */}
       <div className="logo">LOGO</div>
 
-      {/* Center - Navigation Links */}
       <div className={`nav-links ${isOpen ? "open" : ""}`}>
         <a href="#">Home</a>
         <a href="#">About</a>
@@ -43,12 +20,10 @@ const Navigation = () => {
         <a href="#">Contact</a>
       </div>
 
-      {/* Right - Button */}
       <div className="nav-button">
         <button>Get Started</button>
       </div>
 
-      {/* Mobile Menu Toggle */}
       <div className="menu-toggle" onClick={toggleMenu}>
         {isOpen ? "✖" : "☰"}
       </div>
